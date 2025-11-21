55eca1eb29871ddc499cbf77022a3ea972cdc6f7
Lakshay <lakshaybankura393@gmail.com>
2025-11-21 21:12:11 +0530

    Implement About Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import CockTails from './components/CockTails';
+import About from './components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <CockTails />
+        <About />
         {/* <div className="h-dvh bg-black"></div> */}
     </main>
 )
